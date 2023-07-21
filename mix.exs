f6c1deae640246338ce7ee59841bb8c302f58ef7
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-07-21 13:21:51 -0600

    Added Credo

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule AnyHttp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:req, "~> 0.3", optional: true}
     ]
   end
