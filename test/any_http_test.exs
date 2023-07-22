c2ad71717fad2635e2611da464de96a731013652
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-07-21 18:55:52 -0600

    Added tests to AnyHttp

--- test/any_http_test.exs
+++ test/any_http_test.exs
@@ -1,8 +1,57 @@
 defmodule AnyHttpTest do
-  use ExUnit.Case
-  doctest AnyHttp
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert AnyHttp.hello() == :world
+  alias AnyHttp, as: Subject
+
+  test "implements the AnyHttp.Client behaviour" do
+    assert AnyHttp.Client in Keyword.get(Subject.__info__(:attributes), :behaviour, [])
+  end
+
+  test "exposes a function head/2" do
+    function_exported?(Subject, :head, 2)
+  end
+
+  test "exposes a function head/3" do
+    function_exported?(Subject, :head, 3)
+  end
+
+  test "exposes a function get/3" do
+    function_exported?(Subject, :get, 3)
+  end
+
+  test "exposes a function get/4" do
+    function_exported?(Subject, :get, 4)
+  end
+
+  test "exposes a function post/3" do
+    function_exported?(Subject, :post, 3)
+  end
+
+  test "exposes a function post/4" do
+    function_exported?(Subject, :post, 4)
+  end
+
+  test "exposes a function put/3" do
+    function_exported?(Subject, :put, 3)
+  end
+
+  test "exposes a function put/4" do
+    function_exported?(Subject, :put, 4)
+  end
+
+  test "exposes a function patch/3" do
+    function_exported?(Subject, :patch, 3)
+  end
+
+  test "exposes a function patch/4" do
+    function_exported?(Subject, :patch, 4)
+  end
+
+  test "exposes a function delete/3" do
+    function_exported?(Subject, :delete, 3)
+  end
+
+  test "exposes a function delete/4" do
+    function_exported?(Subject, :delete, 4)
   end
 end
