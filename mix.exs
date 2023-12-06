6d5195076025ed9dc2e4abb7544fed217591a6ec
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-12-06 16:42:54 -0600

    Upgraded the library version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AnyHttp.MixProject do
   def project do
     [
       app: :any_http,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [debug_info: Mix.env() == :dev],
