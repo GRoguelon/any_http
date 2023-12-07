ea7822b801d8831e916116f50a938c2d6ab693a8
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2023-12-07 09:51:20 -0600

    Made :httpc default adapter

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,4 @@
 import Config
 
-if config_env() in ~w[dev test]a do
-  # Define default any_http client adapter
-  config :any_http, client_adapter: AnyHttp.Adapters.Httpc
-end
+# Define default any_http client adapter
+config :any_http, client_adapter: AnyHttp.Adapters.Httpc
